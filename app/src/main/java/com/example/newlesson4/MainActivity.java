0d612d060f4bf82770526ad96f3cb0d6679dacdc
arslanbek <arslonbeksheraliev@gmail.com>
2021-03-02 00:19:55 +0600

    lesson_5

--- app/src/main/java/com/example/newlesson4/MainActivity.java
+++ app/src/main/java/com/example/newlesson4/MainActivity.java
@@ -13,119 +13,208 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn9, btn8, btn7, btn6, btn5, btn4, btn3, btn2, btn1, btn0;
         Button btnClear, btnPlusMinus;
-        TextView tvNum;
+        Button btnPlus, btnMinus, btnMultiple, btnDivide, btnEqual;
+        TextView tvNum, tvZapis;
+
+        String firstNum, secondNum,  sign, zapisText;
 
         @Override
         protected void onCreate(Bundle savedIntanceState) {
             super.onCreate(savedIntanceState);
             setContentView(R.layout.activity_main);
-            btn9 = findViewById(R.id.btn9);
-            btn8 = findViewById(R.id.btn8);
-            btn7 = findViewById(R.id.btn7);
-            btn6 = findViewById(R.id.btn6);
-            btn5 = findViewById(R.id.btn5);
-            btn4 = findViewById(R.id.btn4);
-            btn3 = findViewById(R.id.btn3);
-            btn2 = findViewById(R.id.btn2);
-            btn1 = findViewById(R.id.btn1);
-            btn0 = findViewById(R.id.btn0);
 
-            btnClear = findViewById(R.id.btnClear);
-            btnPlusMinus = findViewById(R.id.btnPlusMinus);
+            initViews();
+            numbersMethods();
+            clearPlusMinusMethod();
+            functionMethod();
 
-            tvNum = findViewById(R.id.tv_num);
+    }
+    public void initViews(){
+        btn9 = findViewById(R.id.btn9);
+        btn8 = findViewById(R.id.btn8);
+        btn7 = findViewById(R.id.btn7);
+        btn6 = findViewById(R.id.btn6);
+        btn5 = findViewById(R.id.btn5);
+        btn4 = findViewById(R.id.btn4);
+        btn3 = findViewById(R.id.btn3);
+        btn2 = findViewById(R.id.btn2);
+        btn1 = findViewById(R.id.btn1);
+        btn0 = findViewById(R.id.btn0);
 
-            View.OnClickListener buttonNumbers = new View.OnClickListener() {
+        btnClear = findViewById(R.id.btnClear);
+        btnPlusMinus = findViewById(R.id.btnPlusMinus);
 
-                @Override
-                public void onClick(View v) {
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
 
-                    String previousNumber = tvNum.getText().toString();
-                    String pressedNumber = "";
-                    String finalResult = "";
+        btnMultiple = findViewById(R.id.btnMultiple);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnEqual = findViewById(R.id.btnEqual);
 
-                   switch (v.getId()){
-                       case R.id.btn9:
-                            pressedNumber = "9";
-                           break;
+        tvNum = findViewById(R.id.tv_num);
+        tvZapis = findViewById(R.id.tv_zapis);
+    }
+    public void numbersMethods(){
+        View.OnClickListener buttonNumbers = new View.OnClickListener() {
 
-                       case R.id.btn8:
-                           pressedNumber = "8";
-                           break;
+            @Override
+            public void onClick(View v) {
 
-                       case R.id.btn7:
-                           pressedNumber = "7";
-                           break;
+                String previousNumber = tvNum.getText().toString();
+                String pressedNumber = "";
+                String finalResult = "";
 
-                       case R.id.btn6:
-                           pressedNumber = "6";
-                           break;
+                switch (v.getId()){
+                    case R.id.btn9:
+                        pressedNumber = "9";
+                        break;
 
-                       case R.id.btn5:
-                           pressedNumber = "5";
-                           break;
+                    case R.id.btn8:
+                        pressedNumber = "8";
+                        break;
 
-                       case R.id.btn4:
-                           pressedNumber = "4";
-                           break;
+                    case R.id.btn7:
+                        pressedNumber = "7";
+                        break;
 
-                       case R.id.btn3:
-                           pressedNumber = "3";
-                           break;
+                    case R.id.btn6:
+                        pressedNumber = "6";
+                        break;
 
-                       case R.id.btn2:
-                           pressedNumber = "2";
-                           break;
+                    case R.id.btn5:
+                        pressedNumber = "5";
+                        break;
 
-                       case R.id.btn1:
-                           pressedNumber = "1";
-                           break;
+                    case R.id.btn4:
+                        pressedNumber = "4";
+                        break;
 
-                       case R.id.btn0:
-                           pressedNumber = "0";
-                           break;
-                   }
-                   if(!previousNumber.equals("0")) finalResult = previousNumber + pressedNumber;
-                   else finalResult = pressedNumber;
+                    case R.id.btn3:
+                        pressedNumber = "3";
+                        break;
 
-                    tvNum.setText(finalResult);
+                    case R.id.btn2:
+                        pressedNumber = "2";
+                        break;
+
+                    case R.id.btn1:
+                        pressedNumber = "1";
+                        break;
+
+                    case R.id.btn0:
+                        pressedNumber = "0";
+                        break;
                 }
-            };
+                if(!previousNumber.equals("0")) finalResult = previousNumber + pressedNumber;
+                else finalResult = pressedNumber;
 
-            btn9.setOnClickListener(buttonNumbers);
-            btn8.setOnClickListener(buttonNumbers);
-            btn7.setOnClickListener(buttonNumbers);
-            btn6.setOnClickListener(buttonNumbers);
-            btn5.setOnClickListener(buttonNumbers);
-            btn4.setOnClickListener(buttonNumbers);
-            btn3.setOnClickListener(buttonNumbers);
-            btn2.setOnClickListener(buttonNumbers);
-            btn1.setOnClickListener(buttonNumbers);
-            btn0.setOnClickListener(buttonNumbers);
+                tvNum.setText(finalResult);
+            }
+        };
 
-            View.OnClickListener buttonClearPLusMinus = new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    switch (v.getId()){
-                        case R.id.btnClear:
-                            tvNum.setText("0");
-                            break;
+        btn9.setOnClickListener(buttonNumbers);
+        btn8.setOnClickListener(buttonNumbers);
+        btn7.setOnClickListener(buttonNumbers);
+        btn6.setOnClickListener(buttonNumbers);
+        btn5.setOnClickListener(buttonNumbers);
+        btn4.setOnClickListener(buttonNumbers);
+        btn3.setOnClickListener(buttonNumbers);
+        btn2.setOnClickListener(buttonNumbers);
+        btn1.setOnClickListener(buttonNumbers);
+        btn0.setOnClickListener(buttonNumbers);
+    }
+    public void clearPlusMinusMethod(){
+        View.OnClickListener buttonClearPLusMinus = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.btnClear:
+                        tvNum.setText("0");
+                        tvZapis.setText("");
+                        break;
 
-                        case R.id.btnPlusMinus:
-                            String numText = tvNum.getText().toString();
-                            int num = Integer.parseInt(numText);
+                    case R.id.btnPlusMinus:
+                        String numText = tvNum.getText().toString();
+                        int num = Integer.parseInt(numText);
 
-                            if(num > 0 ) tvNum.setText("-" + numText);
+                        if(num > 0 ) tvNum.setText("-" + numText);
 
-                            else {
-                                num = num * (-1);
-                                tvNum.setText(""+num);
-                            }
-                            break;
-                    }
+                        else {
+                            num = num * (-1);
+                            tvNum.setText(""+num);
+                        }
+                        break;
                 }
-            };
+            }
+        };
+
         btnClear.setOnClickListener(buttonClearPLusMinus);
         btnPlusMinus.setOnClickListener(buttonClearPLusMinus);
+    }
+    public void functionMethod(){
+        View.OnClickListener btnFunctions = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                firstNum = tvNum.getText().toString();
+
+                switch (v.getId()){
+                    case R.id.btnPlus:
+                        sign = "+";
+                        break;
+
+                    case R.id.btnMinus:
+                        sign = "-";
+                        break;
+
+                    case R.id.btnMultiple:
+                        sign = "*";
+                        break;
+
+                    case R.id.btnDivide:
+                        sign = "/";
+                        break;
+                }
+
+                zapisText = firstNum + sign;
+                tvZapis.setText(zapisText);
+                tvNum.setText("0");
+            }
+        };
+
+        btnPlus.setOnClickListener(btnFunctions);
+        btnMinus.setOnClickListener(btnFunctions);
+        btnMultiple.setOnClickListener(btnFunctions);
+        btnDivide.setOnClickListener(btnFunctions);
+
+
+
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                secondNum = tvNum.getText().toString();
+                int num1 = Integer.parseInt(firstNum);
+                int num2 = Integer.parseInt(secondNum);
+                int res = 0;
+
+                if(sign.equals("+")) {
+                    res = num1 + num2;
+
+                }else if(sign.equals("-")){
+                    res = num1 - num2;
+                }
+                else if(sign.equals("*")){
+                    res = num1 * num2;
+                }
+                else if(sign.equals("/")){
+                    res = num1 / num2;
+                }
+                zapisText = firstNum + sign + secondNum + "=" + res;
+
+                tvZapis.setText(zapisText);
+                tvNum.setText("" + res);
+            }
+        });
     }
 }
