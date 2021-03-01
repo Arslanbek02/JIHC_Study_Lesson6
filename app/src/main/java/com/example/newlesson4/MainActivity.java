4abd63d27e9410c12fea2f44f954673e3cd92e8c
arslanbek <arslonbeksheraliev@gmail.com>
2021-03-01 22:52:09 +0600

    lesson_5

--- app/src/main/java/com/example/newlesson4/MainActivity.java
+++ app/src/main/java/com/example/newlesson4/MainActivity.java
@@ -11,85 +11,121 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button9, button8, button7, button6, button5, button4, button3, button2, button1, button0;
-    TextView num;
+        Button btn9, btn8, btn7, btn6, btn5, btn4, btn3, btn2, btn1, btn0;
+        Button btnClear, btnPlusMinus;
+        TextView tvNum;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button9 = findViewById(R.id.btn9);
-        button8 = findViewById(R.id.btn8);
-        button7 = findViewById(R.id.btn7);
-        button6 = findViewById(R.id.btn6);
-        button5 = findViewById(R.id.btn5);
-        button4 = findViewById(R.id.btn4);
-        button3 = findViewById(R.id.btn3);
-        button2 = findViewById(R.id.btn2);
-        button1 = findViewById(R.id.btn1);
-        button0 = findViewById(R.id.btn0);
+        @Override
+        protected void onCreate(Bundle savedIntanceState) {
+            super.onCreate(savedIntanceState);
+            setContentView(R.layout.activity_main);
+            btn9 = findViewById(R.id.btn9);
+            btn8 = findViewById(R.id.btn8);
+            btn7 = findViewById(R.id.btn7);
+            btn6 = findViewById(R.id.btn6);
+            btn5 = findViewById(R.id.btn5);
+            btn4 = findViewById(R.id.btn4);
+            btn3 = findViewById(R.id.btn3);
+            btn2 = findViewById(R.id.btn2);
+            btn1 = findViewById(R.id.btn1);
+            btn0 = findViewById(R.id.btn0);
 
-        num = findViewById(R.id.num);
+            btnClear = findViewById(R.id.btnClear);
+            btnPlusMinus = findViewById(R.id.btnPlusMinus);
 
-        button9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                num.setText('9');
-            }
-        });
-        button8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                num.setText('8');
-            }
-        });
-        button7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                num.setText('7');
-            }
-        });
-        button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                num.setText('6');
-            }
-        });
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                num.setText('5');
-            }
-        });
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                num.setText('4');
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                num.setText('3');
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                num.setText('2');
-            }
-        });
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                num.setText('1');
-            }
-        });
-        button0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                num.setText('0');
-            }
-        });
+            tvNum = findViewById(R.id.tv_num);
+
+            View.OnClickListener buttonNumbers = new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+
+                    String previousNumber = tvNum.getText().toString();
+                    String pressedNumber = "";
+                    String finalResult = "";
+
+                   switch (v.getId()){
+                       case R.id.btn9:
+                            pressedNumber = "9";
+                           break;
+
+                       case R.id.btn8:
+                           pressedNumber = "8";
+                           break;
+
+                       case R.id.btn7:
+                           pressedNumber = "7";
+                           break;
+
+                       case R.id.btn6:
+                           pressedNumber = "6";
+                           break;
+
+                       case R.id.btn5:
+                           pressedNumber = "5";
+                           break;
+
+                       case R.id.btn4:
+                           pressedNumber = "4";
+                           break;
+
+                       case R.id.btn3:
+                           pressedNumber = "3";
+                           break;
+
+                       case R.id.btn2:
+                           pressedNumber = "2";
+                           break;
+
+                       case R.id.btn1:
+                           pressedNumber = "1";
+                           break;
+
+                       case R.id.btn0:
+                           pressedNumber = "0";
+                           break;
+                   }
+                   if(!previousNumber.equals("0")) finalResult = previousNumber + pressedNumber;
+                   else finalResult = pressedNumber;
+
+                    tvNum.setText(finalResult);
+                }
+            };
+
+            btn9.setOnClickListener(buttonNumbers);
+            btn8.setOnClickListener(buttonNumbers);
+            btn7.setOnClickListener(buttonNumbers);
+            btn6.setOnClickListener(buttonNumbers);
+            btn5.setOnClickListener(buttonNumbers);
+            btn4.setOnClickListener(buttonNumbers);
+            btn3.setOnClickListener(buttonNumbers);
+            btn2.setOnClickListener(buttonNumbers);
+            btn1.setOnClickListener(buttonNumbers);
+            btn0.setOnClickListener(buttonNumbers);
+
+            View.OnClickListener buttonClearPLusMinus = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch (v.getId()){
+                        case R.id.btnClear:
+                            tvNum.setText("0");
+                            break;
+
+                        case R.id.btnPlusMinus:
+                            String numText = tvNum.getText().toString();
+                            int num = Integer.parseInt(numText);
+
+                            if(num > 0 ) tvNum.setText("-" + numText);
+
+                            else {
+                                num = num * (-1);
+                                tvNum.setText(""+num);
+                            }
+                            break;
+                    }
+                }
+            };
+        btnClear.setOnClickListener(buttonClearPLusMinus);
+        btnPlusMinus.setOnClickListener(buttonClearPLusMinus);
     }
 }
